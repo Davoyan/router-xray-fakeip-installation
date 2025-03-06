2d13e29ba6fc1521af198975472f266b6da5f06c
Davoyan <chikiti@ya.ru>
2025-03-06 21:09:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,6 +82,7 @@ uci set firewall.@rule[-1].set_mark='0x1'
 uci set firewall.@rule[-1].family='ipv4'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 1'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -93,6 +94,7 @@ uci set firewall.@rule[-1].dest_ip='138.128.136.0/21'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 2'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -104,6 +106,7 @@ uci set firewall.@rule[-1].dest_ip='162.158.0.0/15'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 3'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -115,6 +118,7 @@ uci set firewall.@rule[-1].dest_ip='172.64.0.0/13'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 4'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -126,6 +130,7 @@ uci set firewall.@rule[-1].dest_ip='34.0.0.0/15'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 5'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -137,6 +142,7 @@ uci set firewall.@rule[-1].dest_ip='34.2.0.0/15'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 6'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -148,6 +154,7 @@ uci set firewall.@rule[-1].dest_ip='35.192.0.0/12'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 7'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -159,6 +166,7 @@ uci set firewall.@rule[-1].dest_ip='35.208.0.0/12'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 8'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
@@ -170,6 +178,7 @@ uci set firewall.@rule[-1].dest_ip='5.200.14.128/25'
 uci set firewall.@rule[-1].dest_port='50000-65535'
 
 uci add firewall rule
+uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Discord Marking 9'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
