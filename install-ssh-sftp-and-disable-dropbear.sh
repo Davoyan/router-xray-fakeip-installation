7c68bb8c8763b969dc339fed0763debd51d49ba8
Davoyan <chikiti@ya.ru>
2025-03-21 12:38:17 +0300

    Update install-ssh-sftp-and-disable-dropbear.sh

--- install-ssh-sftp-and-disable-dropbear.sh
+++ install-ssh-sftp-and-disable-dropbear.sh
@@ -4,12 +4,12 @@ opkg install openssh-server openssh-sftp-server
 
 CONFIG_FILE="/etc/ssh/sshd_config"
 
-# Расскомментируем и изменяем нужные параметры
+
 sed -i -E 's/^#?(Port) .*/\1 22/' $CONFIG_FILE
 sed -i -E 's/^#?(PermitRootLogin) .*/\1 yes/' $CONFIG_FILE
 sed -i -E 's/^#?(PasswordAuthentication) .*/\1 yes/' $CONFIG_FILE
 
-# Перезапускаем SSH для применения изменений
+/etc/init.d/sshd enable
 /etc/init.d/sshd restart
 /etc/init.d/dropbear stop
 /etc/init.d/dropbear disable
